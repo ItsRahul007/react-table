4503e4163b65d61725dfd270b0fcbb8679172b75
rahulMile9 <rahul@mile9.io>
2023-12-15 20:59:12 +0530

    some updates

--- src/app/expandable/page.tsx
+++ src/app/expandable/page.tsx
@@ -11,6 +11,8 @@ import {
   getExpandedRowModel,
   ColumnDef,
   flexRender,
+  ColumnOrderState,
+  createColumnHelper,
 } from '@tanstack/react-table'
 import { makeData, Person } from './makeData'
 import DATA from "../data.json";
@@ -18,11 +20,83 @@ import VisibilityCompo from '../compo/Vsibility';
 
 const Page: React.FC = () => {
   const rerender = useReducer(() => ({}), {})[1];
+  const columnHelper = createColumnHelper<Person>();
+
+  // const columns = useMemo<ColumnDef<Person>[]>(
+  //   () => [
+  //     {
+  //       accessorKey: 'firstName',
+  //       header: ({ table }) => (
+  //         <>
+  //           <button
+  //             {...{
+  //               onClick: table.getToggleAllRowsExpandedHandler(),
+  //             }}
+  //           >
+  //             {table.getIsAllRowsExpanded() ? '👇' : '👉'}
+  //           </button>{' '}
+  //           First Name
+  //         </>
+  //       ),
+  //       cell: ({ row, getValue }) => (
+  //         <div
+  //           style={{
+  //             paddingLeft: `${row.depth * 2}rem`,
+  //           }}
+  //         >
+  //           <>
+  //             {row.getCanExpand() && (
+  //               <button
+  //                 {...{
+  //                   onClick: row.getToggleExpandedHandler(),
+  //                   style: { cursor: 'pointer' },
+  //                 }}
+  //               >
+  //                 {row.getIsExpanded() ? '👇' : '👉'}
+  //               </button>
+  //             )}{' '}
+  //             {getValue()}
+  //           </>
+  //         </div>
+  //       ),
+  //       footer: props => props.column.id,
+  //     },
+  //     {
+  //       accessorFn: row => row.lastName,
+  //       id: 'lastName',
+  //       cell: info => info.getValue(),
+  //       header: () => <span>Last Name</span>,
+  //       footer: props => props.column.id,
+  //     },
+  //     // {
+  //     //   accessorKey: 'age',
+  //     //   header: () => 'Age',
+  //     //   footer: props => props.column.id,
+  //     // },
+  //     {
+  //       accessorKey: 'visits',
+  //       header: () => <span>Visits</span>,
+  //       footer: props => props.column.id,
+  //     },
+  //     {
+  //       accessorKey: 'status',
+  //       header: 'Status',
+  //       footer: props => props.column.id,
+  //     },
+  //     {
+  //       accessorKey: 'progress',
+  //       header: 'Profile Progress',
+  //       footer: props => props.column.id,
+  //     }
+  //   ],
+  //   []
+  // )
 
   const columns = useMemo<ColumnDef<Person>[]>(
     () => [
       {
         accessorKey: 'firstName',
+        id: 'firstName',
         header: ({ table }) => (
           <>
             <button
@@ -60,14 +134,9 @@ const Page: React.FC = () => {
       },
       {
         accessorFn: row => row.lastName,
-        id: 'lastName',
+        accessorKey: 'lastName',
         cell: info => info.getValue(),
         header: () => <span>Last Name</span>,
-        footer: props => props.column.id,
-      },
-      {
-        accessorKey: 'age',
-        header: () => 'Age',
         footer: props => props.column.id,
       },
       {
@@ -76,17 +145,21 @@ const Page: React.FC = () => {
         footer: props => props.column.id,
       },
       {
-        accessorKey: 'status',
-        header: 'Status',
-        footer: props => props.column.id,
+        header: "More Information",
+        columns: [
+          {
+            accessorKey: 'status',
+            header: 'Status',
+            footer: props => props.column.id,
+          },
+          {
+            accessorKey: 'progress',
+            header: 'Profile Progress',
+            footer: props => props.column.id,
+          }
+        ]
       },
-      {
-        accessorKey: 'progress',
-        header: 'Profile Progress',
-        footer: props => props.column.id,
-      }
-
-    ],
+      ],
     []
   )
 
@@ -94,32 +167,48 @@ const Page: React.FC = () => {
   const [expanded, setExpanded] = useState<ExpandedState>({});
   const [columnVisibility, setColumnVisibility] = useState({});
 
+  const [columnOrder, setColumnorder] = useState<ColumnOrderState>(
+    columns.map(column => column.id as string)
+  );
+
   const table = useReactTable({
     data: DATA,
     columns,
     columnResizeMode: "onChange",
     state: {
+      columnOrder,
       expanded,
-      columnVisibility
+      columnVisibility,
     },
     onExpandedChange: setExpanded,
     getSubRows: row => row.subRows,
     getCoreRowModel: getCoreRowModel(),
     getExpandedRowModel: getExpandedRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
+    onColumnOrderChange: setColumnorder,
   })
 
   return (
     <div className="p-2">
       <div className="h-2" />
-      <VisibilityCompo table={table} />
-      <div className='divTable'>
+      <div className='flex'>
+        <VisibilityCompo table={table} />
+        <div className="flex flex-row gap-5 h-10 mb-6">
+          <button onClick={() => { setColumnorder(['status']) }} className="border-2 p-1">
+            Change Order
+          </button>
+          <button onClick={() => { setColumnorder(columns.map(column => column.id as string)) }} className="border-2 p-1">
+            Reset Order
+          </button>
+        </div>
+      </div>
+      <div className='border border-slate-200 w-fit'>
         <div>
           {table.getHeaderGroups().map(headerGroup => (
-            <div className='tr' key={headerGroup.id}>
+            <div className='flex w-fit h-8' key={headerGroup.id}>
               {headerGroup.headers.map(header => {
                 return (
-                  <div className='th' key={header.id} {...{
+                  <div className='border border-slate-200 p-[2px] px-1 relative font-bold text-center h-8' key={header.id} {...{
                     // key: header.id,
                     colSpan: header.colSpan,
                     style: {
